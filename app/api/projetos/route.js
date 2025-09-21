7f7ada22d16f4984bb054241f1f1dc1be3a7346c
José Isaac <joseisaacnascimento@gmail.com>
2025-09-21 20:59:40 -0300

    feat: adicionar try/catch na função de buscar projetos

--- app/api/projetos/route.js
+++ app/api/projetos/route.js
@@ -1,6 +1,12 @@
 import prisma from "@/lib/prisma";
 
+// busca todos os projetos
 export async function GET() {
-  const projetos = await prisma.projeto.findMany();
-  return Response.json(projetos);
+  try {
+    const projetos = await prisma.projeto.findMany();
+    return Response.json(projetos);
+  } catch (error) {
+    console.error(error);
+    return new Response("Erro ao buscar projetos", { status: 500 });
+  }
 }
