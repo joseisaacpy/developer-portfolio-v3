b990446dcec1b6b9afce99970b30110a79afe88f
José Isaac <joseisaacnascimento@gmail.com>
2025-09-27 15:31:50 -0300

    style: adicionar sombra azul ao passar mouse sobre experiência - remover index de dentro do span

--- Components/Experiencia/page.jsx
+++ Components/Experiencia/page.jsx
@@ -16,15 +16,18 @@ export default function Experiencias() {
               // Article
               <article
                 key={index}
-                className="relative bg-white p-6 rounded-lg shadow hover:shadow-lg transition-all"
+                className="relative bg-white p-6 rounded-lg shadow hover:shadow-lg hover:shadow-blue-500 transition-all"
               >
                 {/* Span para bolinha */}
-                <span className="absolute px-3 py-1 top-[-10px] left-[-13px] rounded-full bg-gradient-to-tl from-blue-300 to-blue-900 font-bold text-white shadow-md">
-                  {index + 1} {/* +1 para não iniciar no 0 */}
+                <span className="absolute w-8 h-8 top-[-8px] left-[-10px] rounded-full bg-gradient-to-tl from-blue-300 to-blue-900 font-bold text-white shadow-md">
+                  {/* {index - experiencias.length + 1}{" "} */}
+                  {/* +1 para não iniciar no 0 */}
                 </span>
                 <h3 className="font-bold">{exp.cargo}</h3>
                 <p>{exp.empresa}</p>
-                <p className="text-sm">{exp.periodo}</p>
+                <p className="text-sm">
+                  {exp.admissao} - {exp.fim == "" ? "Atual" : exp.fim}
+                </p>
                 <p>{exp.descricao}</p>
               </article>
             );
