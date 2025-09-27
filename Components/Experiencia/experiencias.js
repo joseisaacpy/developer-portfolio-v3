358705ae2931e9631f09e9486717c190d98ae5d0
José Isaac <joseisaacnascimento@gmail.com>
2025-09-27 15:32:36 -0300

    refacor: criar período de admissão e fim da experiência ao invés de ter tudo junto

--- Components/Experiencia/experiencias.js
+++ Components/Experiencia/experiencias.js
@@ -1,22 +1,25 @@
 export const experiencias = [
   {
     empresa: "Tecnotel",
-    cargo: "Suporte Técnico",
-    periodo: "08/2025 – 09/2025",
+    cargo: "Estágio - Suporte Técnico",
+    admissao: "08/2025",
+    fim: "09/2025",
     descricao:
       "Atendimento ao usuário, abertura e resolução de chamados técnicos.",
   },
   {
     empresa: "Grupo Adriano Cobuccio",
     cargo: "Aprendiz - Assistente de Dados",
-    periodo: "11/2024 – Atual",
+    admissao: "11/2024",
+    fim: "",
     descricao:
       "Apoio na análise de dados e acompanhamento de rotinas administrativas.",
   },
   {
     empresa: "R Damásio",
     cargo: "Aprendiz - Logística",
-    periodo: "04/2023 – 04/2024",
+    admissao: "04/2023",
+    fim: "04/2024",
     descricao: "Suporte a processos logísticos e atividades administrativas.",
   },
 ];
