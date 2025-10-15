65734f9b869f58113556ceced6cdc83d0cd18cf7
José Isaac <joseisaacnascimento@gmail.com>
2025-10-15 14:29:38 -0300

    feat: ordenar projetos pela data de criação na rota GET

--- app/api/projetos/route.js
+++ app/api/projetos/route.js
@@ -3,9 +3,16 @@ import prisma from "@/lib/prisma";
 // busca todos os projetos
 export async function GET() {
   try {
-    const projetos = await prisma.projeto.findMany();
+    // busca todos os projetos ordenando pela data de criação
+    const projetos = await prisma.projeto.findMany({
+      orderBy: {
+        data_criacao: "desc",
+      },
+    });
+    // retorna os projetos
     return Response.json(projetos);
   } catch (error) {
+    // caso ocorra algum erro
     console.error(error);
     return new Response("Erro ao buscar projetos", { status: 500 });
   }
