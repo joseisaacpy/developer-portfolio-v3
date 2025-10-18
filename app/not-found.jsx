1c9fd55f64e9cd88f3722b1d66b0e7212c040c73
José Isaac <joseisaacnascimento@gmail.com>
2025-10-18 11:57:11 -0300

    feat: criar página de erro/not-found

---
+++ app/not-found.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <section className="min-h-screen w-screen flex items-center justify-center text-center bg-slate-800 text-white">
+      <h1 className="text-2xl md:text-3xl font-bold">404 - Page Not Found</h1>
+    </section>
+  );
+}
