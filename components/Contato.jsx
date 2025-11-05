1677544bfe08ed633e5f7092887f3ba87cea88a8
José Isaac <joseisaacnascimento@gmail.com>
2025-11-05 15:19:42 -0300

    style(contato): ajustar espaçamento entre elementos

--- components/Contato.jsx
+++ components/Contato.jsx
@@ -11,45 +11,45 @@ export default function Contato() {
     toast.success("E-mail copiado com sucesso");
   };
   return (
-    <section className="bg-black flex flex-col items-center justify-center p-4">
+    <section className="bg-black flex flex-col items-center justify-center p-4 md:p-10 gap-6">
       <h4 className="text-2xl md:text-3xl font-bold text-center text-white">
         Conexões constroem pontes — vamos conversar!
       </h4>
-      <div className="p-2 flex flex-col items-center justify-center w-full md:max-w-[80%]">
+      <div className="flex flex-col items-center justify-center w-full md:max-w-[80%]">
         <Image
           src={japanImg}
           loading="lazy"
           alt="Imagem de um lugar japonês"
-          className="w-full max-h-[200px] md:max-h-[350px] object-cover rounded-t-2xl"
+          className="w-full h-auto max-h-[350px] object-cover rounded-t-2xl"
         />
-        <div className="p-2 bg-white flex items-center justify-center gap-4 rounded-b-2xl w-full">
+        <div className="p-2 bg-white flex items-center justify-center gap-6 rounded-b-2xl w-full">
           <a
             target="_blank"
             href="https://www.linkedin.com/in/jos%C3%A9-isaac-nascimento/"
             className="hover:scale-105 transition duration-300 hover:text-blue-500"
           >
-            <FaLinkedin className="text-3xl" />
+            <FaLinkedin className="text-4xl md:text-3xl" />
           </a>
           <a
             target="_blank"
             href="https://github.com/joseisaacpy"
             className="hover:scale-105 transition duration-300 hover:text-blue-500"
           >
-            <FaGithub className="text-3xl" />
+            <FaGithub className="text-4xl md:text-3xl" />
           </a>
           <a
             target="_blank"
             href="https://www.instagram.com/j0se_isaacsn/"
             className="hover:scale-105 transition duration-300 hover:text-blue-500"
           >
-            <FaInstagram className="text-3xl" />
+            <FaInstagram className="text-4xl md:text-3xl" />
           </a>
           <a
             onClick={handleEmailClick}
             title="Copiar e-mail"
             className="cursor-pointer hover:scale-105 transition duration-300 hover:text-blue-500"
           >
-            <FaEnvelope className="text-3xl" />
+            <FaEnvelope className="text-4xl md:text-3xl" />
           </a>
         </div>
       </div>
