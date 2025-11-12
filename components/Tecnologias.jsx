caaa69be3977514ca10e72d0efd5e302000f47ae
José Isaac <joseisaacnascimento@gmail.com>
2025-11-12 16:30:50 -0300

    feat(section tecnologias): criar função para filtrar categoria de tecnologias

--- components/Tecnologias.jsx
+++ components/Tecnologias.jsx
@@ -1,9 +1,24 @@
+"use client";
+
+// Importa hooks
+import { useState } from "react";
+
 // Importa o next/image
 import Image from "next/image";
 
+// Importa array de tecnologias
 import tecnologias from "@/data/tecnologias";
 
+// Array de categorias de tecnologias
+const categorias = [
+  "Todas",
+  ...new Set(tecnologias.map((tecnologia) => tecnologia.categoria)),
+];
+
 export default function Tecnologias() {
+  const [categoriaFiltrada, setCategoriaFiltrada] = useState("Todas");
+  const [tecnologiaFiltrada, setTecnologiaFiltrada] = useState(tecnologias);
+
   return (
     <section
       id="tecnologias"
@@ -14,12 +29,43 @@ export default function Tecnologias() {
           Tecnologias e Ferramentas
         </h2>
 
+        {/* Botões para filtrar as tecnologias */}
+        <div className="flex flex-row flex-wrap items-center justify-center gap-2 mb-5">
+          {categorias.map((categoria) => {
+            return (
+              <button
+                key={categoria}
+                aria-pressed={categoria === categoriaFiltrada}
+                onClick={() => {
+                  setCategoriaFiltrada(categoria);
+                  if (categoria === "Todas") {
+                    setTecnologiaFiltrada(tecnologias);
+                  } else {
+                    setTecnologiaFiltrada(
+                      tecnologias.filter(
+                        (tecnologia) => tecnologia.categoria === categoria
+                      )
+                    );
+                  }
+                }}
+                className={`p-2 bg-blue-600 rounded-2xl text-white cursor-pointer hover:scale-105 transition-all ${
+                  categoria === categoriaFiltrada
+                    ? "opacity-100"
+                    : "opacity-50"
+                }`}
+              >
+                {categoria}
+              </button>
+            );
+          })}
+        </div>
+
         <div
           id="container-tecnologias"
           className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 justify-center"
         >
-          {/* Renderiza as tecnologias */}
-          {tecnologias.map((tecnologia, index) => {
+          {/* Renderiza as tecnologias filtradas */}
+          {tecnologiaFiltrada.map((tecnologia, index) => {
             return (
               <div
                 key={tecnologia.nome}
@@ -33,7 +79,7 @@ export default function Tecnologias() {
                   width={80} // precisa manter algum valor mínimo para otimização
                   height={80}
                   className="w-16 h-auto"
-                  alt={`Ícone da linguagem ${tecnologia.nome}`}
+                  alt={`Ícone representando ${tecnologia.nome}`}
                 />
                 <p className="mt-2 text-sm font-medium">{tecnologia.nome}</p>
               </div>
