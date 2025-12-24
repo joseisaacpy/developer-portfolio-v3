5a9adb997f576c735a8b109170ccc5dc3bbe07df
José Isaac <joseisaacnascimento@gmail.com>
2025-12-24 14:15:01 -0300

    refactor(loader): corrigir a tag de Image no loader

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,19 +1,11 @@
-// Image
-import Image from "next/image";
-
-// Logo
-import logoImg from "@/public/images/dev-jose.webp";
-
 export default function Loader() {
   return (
-    <div className="min-h-full min-w-full z-50 inset-0 fixed bg-gradient-to-br from-black to-blue-500 flex flex-col gap-2 items-center justify-center transition-all duration-600">
+    <div className="min-h-full min-w-full z-50 inset-0 fixed bg-gradient-to-br from-black to-blue-default flex flex-col gap-2 items-center justify-center transition-all duration-600">
       {/* Logo */}
-      <Image
-        className="h-20 w-20 animate-spin rounded-full border-2 border-white"
-        src={logoImg}
+      <img
+        src="/images/dev-jose.webp"
         alt="Logo de José Isaac"
-        width={50}
-        height={50}
+        className="h-20 w-20 animate-spin rounded-full border-2 border-white"
       />
       {/* Texto de carregamento */}
       <h1 className="text-lg text-white font-bold z-10">Carregando...</h1>
