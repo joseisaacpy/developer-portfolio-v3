5a3a365e27f586f6f43e4f1e067fba7e7f1f4020
José Isaac <joseisaacnascimento@gmail.com>
2025-12-30 22:12:27 -0300

    feat(type): criar type para projetos

---
+++ types/projeto.ts
@@ -0,0 +1,10 @@
+export type ProjetoProps = {
+  id: number;
+  nome: string;
+  descricao: string;
+  data_criacao: string;
+  tecnologias: string[];
+  linkDemo: string;
+  linkRepo: string;
+  status: boolean;
+};
